87fe1db42e2ab00e8d5603ab0538731d1c3377b8
tony89-FR <tonyberthe89500@outlook.fr>
2026-06-20 16:16:41 +0200

    Modify moderator IDs and add test moderator
    Updated moderator IDs and added a test moderator.

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ const STAFF_ROLES = [
     },
     {
         name: "🟧 Modérateur",
-        id: "1478757828637360251"
+        id: "1448334482867355832"
     }
+    {
+        name: "🟧 Modérateur test",
+        id: "1478757828637360251"
+    },
 ];
 
 const client = new Client({
