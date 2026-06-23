326d941127c2d3e897965d864ecf11126714aff8
tony89-FR <tonyberthe89500@outlook.fr>
2026-06-23 15:15:31 +0200

    Add bot configuration with roles and guild ID
    Added configuration for bot roles and guild ID.

--- config/config.js
+++ config/config.js
@@ -1,1 +1,45 @@
 // Configuration du bot
+module.exports = {
+
+    GUILD_ID: "1447249368783523942",
+
+    STAFF_ROLES: [
+
+        {
+            name: "👑 Fondateur",
+            id: "1447263311375761460"
+        },
+
+        {
+            name: "✨ Créateur",
+            id: "1447264238279196724"
+        },
+
+        {
+            name: "🛡 Co-Fondateur",
+            id: "1485266632270942288"
+        },
+
+        {
+            name: "💪 Bras droit",
+            id: "1448333177947947109"
+        },
+
+        {
+            name: "🟨 Chef Modérateur",
+            id: "1448333720946737368"
+        },
+
+        {
+            name: "🟧 Modérateur",
+            id: "1448334482867355832"
+        },
+
+        {
+            name: "🟪 Modérateur test",
+            id: "1478757828637360251"
+        }
+
+    ]
+
+};
