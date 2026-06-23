846d659c543d902195d417357faa1964c80b9085
tony89-FR <tonyberthe89500@outlook.fr>
2026-06-23 15:18:26 +0200

    Refactor guild and staff roles configuration
    Refactor to import GUILD_ID and STAFF_ROLES from config file, removing hardcoded values.

--- index.js
+++ index.js
@@ -15,45 +15,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-const GUILD_ID = "1447249368783523942";
-
-const STAFF_ROLES = [
-    {
-        name: "👑 Fondateur",
-        id: "1447263311375761460"
-    },
-    {
-        name: "✨ Créateur",
-        id: "1447264238279196724"
-    },
-    {
-        name: "🛡 Co-Fondateur",
-        id: "1485266632270942288"
-    },
-    {
-        name: "💪 Bras droit",
-        id: "1448333177947947109"
-    },
-    {
-        name: "🟨 Chef Modérateur",
-        id: "1448333720946737368"
-    },
-    {
-        name: "🟧 Modérateur",
-        id: "1448334482867355832"
-    },
-    {
-        name: "🟧 Modérateur test",
-        id: "1478757828637360251"
-    },
-];
-
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers
-    ]
-});
+const { GUILD_ID, STAFF_ROLES } = require("./config/config");
 
 let eventsCache = [];
 let staffCache = [];
