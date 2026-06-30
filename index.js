80c281043b37498d2bc62a931d940a75616e07b1
tony89-FR <tonyberthe89500@outlook.fr>
2026-06-30 15:40:04 +0200

    Refactor updateEvents function and its calls
    Refactor updateEvents function to use client parameter and streamline event fetching.

--- index.js
+++ index.js
@@ -24,49 +24,22 @@ const PORT = process.env.PORT || 3000;
 
 const { GUILD_ID, STAFF_ROLES } = require("./config/config");
 const { updateStaff } = require("./config/staff");
+const { updateEvents } = require("./services/events");
 
 let eventsCache = [];
 let staffCache = [];
 
-async function updateEvents() {
-
-    try {
-
-        const guild = client.guilds.cache.get(GUILD_ID);
-
-        if (!guild) return;
-
-        const events = await guild.scheduledEvents.fetch();
-
-        eventsCache = [...events.values()].map(event => ({
-
-            name: event.name,
-            description: event.description || "",
-            startTime: event.scheduledStartTimestamp
-
-        }));
-
-        console.log(`📅 ${eventsCache.length} événement(s) synchronisé(s)`);
-
-    } catch (err) {
-
-        console.error("Erreur updateEvents :", err);
-
-    }
-
-}
-
 client.once(Events.ClientReady, async () => {
 
     console.log(`✅ Connecté : ${client.user.tag}`);
-
-    await updateEvents();
+    
+    eventsCache = await updateEvents(client, GUILD_ID);
     staffCache = await updateStaff(client, GUILD_ID, STAFF_ROLES);
 
     setInterval(async () => {
-        await updateEvents();
-    }, 5 * 60 * 1000);
-
+    eventsCache = await updateEvents(client, GUILD_ID);
+}, 5 * 60 * 1000);
+    
     setInterval(async () => {
         staffCache = await updateStaff(client, GUILD_ID, STAFF_ROLES);
     }, 30 * 60 * 1000);
